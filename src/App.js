6ad01a20a36b48fe2074078171142e08fef415cf
yangzhengca <yangzhengca>
2021-10-20 15:29:52 -0600

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+import Calendar, { CalendarDayHeader } from "./calendar.js";
 
-function App() {
+export default function App() {
+  const [yearAndMonth, setYearAndMonth] = useState([2021, 9]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar
+        yearAndMonth={yearAndMonth}
+        onYearAndMonthChange={setYearAndMonth}
+        renderDay={(calendarDayObject) => (
+          <div>
+            <CalendarDayHeader calendarDayObject={calendarDayObject} />
+          </div>
+        )}
+      />
     </div>
   );
 }
-
-export default App;
